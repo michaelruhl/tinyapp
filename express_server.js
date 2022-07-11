567128c3d632d60bae99b0dc56705f5cb77c04ea
Michael Ruhl <michaelruhl14@hotmail.com>
2022-07-11 23:46:21 +0000

    added fancy form feature

--- express_server.js
+++ express_server.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  result = [];
+  for (i = 0; i < 6; i++) {
+
+    result.push(Math.ceil(Math.random() * 6))
+  }
+  let string = `${result[0]}${result[1]}${result[2]}${result[3]}${result[4]}${result[5]}`
+  console.log(string)
+  return string;
+}
+
+generateRandomString()
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get('/', (req, res) => {
   res.send('Hello!');
@@ -17,15 +31,25 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id]
-  
+  const longURL = urlDatabase[id];
+
   const templateVars = { id, longURL };
   res.render("urls_show", templateVars);
 });
